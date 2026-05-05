258ebdbff6e499116e3a2b464a483dfc7f96e0a8
fedepanun-glitch <fedepanun@gmail.com>
2026-05-05 09:35:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("hola");
+var express = require("express");
+var app = express();
+app.use(express.urlencoded({ extended: false }));
+app.get("/", function(req, res) { res.send("Bot OK"); });
+app.listen(process.env.PORT || 3000, function() { console.log("Servidor andando"); });
